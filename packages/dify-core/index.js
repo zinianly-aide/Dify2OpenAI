9063d7b77d3fa15b02b8c7fea36f2f623780171e
zinianly-aide <zinianly@gmail.com>
2026-08-30 05:06:32 +0800

    refactor: export shared dify core

---
+++ packages/dify-core/index.js
@@ -0,0 +1,5 @@
+export * from './canonical.js';
+export * from './conversation-manager.js';
+export * from './tool-schema-registry.js';
+export * from './tool-execution-ledger.js';
+export * from './dify-client.js';
